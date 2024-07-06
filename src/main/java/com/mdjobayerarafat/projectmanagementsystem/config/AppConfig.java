77064afd5c1a9d69121fb8876376f3b7895c95ec
Jubayer Arafat <mdjobayerarafat@gmail.com>
2024-07-06 23:57:57 +0600

    start2

--- src/main/java/com/mdjobayerarafat/projectmanagementsystem/config/AppConfig.java
+++ src/main/java/com/mdjobayerarafat/projectmanagementsystem/config/AppConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -15,7 +16,12 @@ public class AppConfig {
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http)throws Exception{
         http.sessionManagement(Management -> Management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests(Authorize->Authorize.requestMatchers("/api?**"))
+                .authorizeHttpRequests(Authorize->Authorize.requestMatchers("/api?**").authenticated()
+                        .anyRequest().permitAll())
+                .addFilterBefore(new JwtTokenValidator(), BasicAuthenticationFilter.class)
+                .csrf(csrf -> csrf.disable())
+                .cors(cors->.configurationSource(corsConfigrationSource()));
+                return null;
     }
 }
 
