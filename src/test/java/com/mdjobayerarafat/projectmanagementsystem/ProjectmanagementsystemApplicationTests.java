87164582fe31d626666bc05a057e6c5be0b03450
Jubayer Arafat <mdjobayerarafat@gmail.com>
2024-07-06 23:51:07 +0600

    start

---
+++ src/test/java/com/mdjobayerarafat/projectmanagementsystem/ProjectmanagementsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mdjobayerarafat.projectmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectmanagementsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
