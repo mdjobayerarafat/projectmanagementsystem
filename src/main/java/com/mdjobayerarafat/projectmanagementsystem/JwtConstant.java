5e255aa252e5a5b4a531c0459508bff4d02e5547
Jubayer Arafat <mdjobayerarafat@gmail.com>
2024-07-11 14:42:50 +0600

    jwt up

---
+++ src/main/java/com/mdjobayerarafat/projectmanagementsystem/JwtConstant.java
@@ -0,0 +1,7 @@
+package com.mdjobayerarafat.projectmanagementsystem;
+
+public class JwtConstant {
+    public static final String SECRETE_KEY="dsfdgrfghr dfgrgrrefgrdsdewr fghrfgtr hjtrtgessfdgvfbtyt";
+
+    public static final String JWT_HEADER="Authorization";
+}
